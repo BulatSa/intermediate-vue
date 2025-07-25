fa24c60b45b4a184b892fa9401f0ffd2d54bebc0
Bulat <sadikovba@gmail.com>
2025-07-25 14:13:18 +0300

    add base input

---
+++ app/src/views/SandboxPage.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>helko</h1>
+<BaseTextInput placeholder="Hello" />
+</template>
+
+<script setup lang="ts">
+import BaseTextInput from '@/components/BaseTextInput.vue';
+
+
+</script>
